6f8fdffdce4cfe150887bf7171dcc7ede3cfaef2
Andrea Chavarria <andrea.chavarria@unah.hn>
2021-07-27 20:24:24 -0600

    agregando correcciones

--- TiendaOnline-main/TiendaOnline/TiendaOnline.Web/Controllers/HomeController.cs
+++ TiendaOnline-main/TiendaOnline/TiendaOnline.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TiendaOnline.BL;
 
 namespace TiendaOnline.Web.Controllers
 {
@@ -11,7 +12,10 @@ namespace TiendaOnline.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var productosBL = new ProductosBL();
+            var listadeproductos = productosBL.ObtenerProductos();
+
+            return View(listadeproductos);
         }
     }
 }
